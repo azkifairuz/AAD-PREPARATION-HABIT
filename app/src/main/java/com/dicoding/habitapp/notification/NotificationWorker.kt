73ad3c20477046dafb1c77b79d0ef4d18688a0a3
azkifairuz <azkiajmal@gmail.com>
2023-12-08 13:53:40 +0700

    notification worker

--- app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -1,11 +1,20 @@
 package com.dicoding.habitapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.detail.DetailHabitActivity
 import com.dicoding.habitapp.utils.HABIT_ID
 import com.dicoding.habitapp.utils.HABIT_TITLE
+import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
 
 class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
@@ -16,9 +25,49 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
         val prefManager = androidx.preference.PreferenceManager.getDefaultSharedPreferences(applicationContext)
         val shouldNotify = prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
 
-        //TODO 12 : If notification preference on, show notification with pending intent
+        //  TODO 12 : If notification preference on, show notification with pending intent
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if(shouldNotify) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val notificationChannel = NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    applicationContext.getString(R.string.notify_channel_name),
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                notificationManager.createNotificationChannel(notificationChannel)
+            }
 
+            val pendingIntent = getPendingIntent(habitId)
+            val notification =
+                NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+                    .setContentTitle(habitTitle)
+                    .setContentText(applicationContext.getString(R.string.notify_content))
+                    .setSmallIcon(R.drawable.ic_notifications)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .build()
+            notificationManager.notify(0,notification)
+        }
         return Result.success()
     }
+
+    private fun getPendingIntent(habitId: Int): PendingIntent? {
+        val intent = Intent(applicationContext, DetailHabitActivity::class.java).apply {
+            putExtra(HABIT_ID, habitId)
+        }
+        return TaskStackBuilder.create(applicationContext).run {
+            addNextIntentWithParentStack(intent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getPendingIntent(
+                    0,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+            } else {
+                getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+        }
+    }
+}
 
 }
--- app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
@@ -7,8 +7,12 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.utils.DarkMode
+import java.util.Locale
+
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -48,6 +52,11 @@ class SettingsActivity : AppCompatActivity() {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
             //TODO 11 : Update theme based on value in ListPreference
+            val themePreference = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+            themePreference?.setOnPreferenceChangeListener{_,newValue ->
+                val selectedTheme = DarkMode.valueOf((newValue as String).uppercase(Locale.US))
+                updateTheme(selectedTheme.value)
+            }
         }
 
         private fun updateTheme(mode: Int): Boolean {
